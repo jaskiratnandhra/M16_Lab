db61ad6acd5c8d09817821e5f4c7185d0ae8b483
jaskiratnandhra <jaskiratnandhra@gmail.com>
2024-05-09 03:45:27 -0700

    M16_Lab

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import {} from 'dotenv/config'
+import routes from './routes/routes.js'
+import connectDB from './db/connect.js'
+import bodyParser from 'body-parser'
+import path from 'path'
+
+
+const app = express()
+
+// Specifically set the public folder as the static folder
+app.use(express.static('public'))
+
+// Without this middleware, we would not be able to decipher the 
+// data being sent to Node.js from an application. 
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+// LOAD ROUTES INTO OUR MAIN FILE
+// if the below route is detected, then load our routes from routes.js
+app.use('/', routes)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'))
+})
+
+const PORT = process.env.PORT || 5000
+
+const init = async () => {
+    try {
+        await connectDB(process.env.DB)
+        console.log('Connected to the database...')
+
+        app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+init()
